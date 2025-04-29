45f33d2c278aa71fae502476332c26712538eb75
anna leme <162629023+annabialeme@users.noreply.github.com>
2025-04-29 08:35:57 -0300

    feat: create editora controller

---
+++ src/controllers/editoraController.js
@@ -0,0 +1,56 @@
+const editoraModel = require("../models/editoraModel");
+
+const getAllEditoras = async (req, res) => {
+    try {
+        const editoras = await editoraModel.getAllEditoras();
+        res.json(editoras);
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao buscar editora." });
+    }
+};
+
+const getEditora = async (req, res) => {
+    try {
+        const editora = await editoraModel.getEditoraById(req.params.id);
+        if (!editora) {
+            return res.status(404).json({ message: "Editora não encontrada." });
+        }
+        res.json(editora);
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao buscar editora." });
+    }
+};
+
+const createEditora = async (req, res) => {
+    try {
+        const { name} = req.body;
+        const newEditora = await editoraModel.createEditora(name);
+        res.status(201).json(newEditora);
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao criar editora." });
+    }
+};
+
+const updateEditora = async (req, res) => {
+    try {
+        const { name } = req.body;
+        const updatedEditora = await editoraModel.updateEditora(req.params.id, name);
+        if (!updatedEditora) {
+            return res.status(404).json({ message: "Editora não encontrada." });
+        }
+        res.json(updatedEditora);
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao atualizar editora." });
+    }
+};
+
+const deleteEditora = async (req, res) => {
+    try {
+        const message = await editoraModel.deleteEditora(req.params.id);
+        res.json(message);
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao deletar editora." });
+    }
+};
+
+module.exports = { getAllEditoras, getEditora, createEditora, updateEditora, deleteEditora };
